845b896ae570d8f847bb94d8007e4a5b7ecf73a7
Alex Tuley <alex.p.tuley@gmail.com>
2021-03-15 12:14:03 -0400

    Refactor EventList component to use hooks

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -1,73 +1,54 @@
 /* eslint-disable camelcase */
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-class EventList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: '',
-    };
+const EventList = ({ activeId, events }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchInput = useRef(null);
+  const filteredEvents = events
+    .filter((el) => matchSearchTerm(el, searchTerm))
+    .sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
 
-    this.searchInput = React.createRef();
-    this.updateSearchTerm = this.updateSearchTerm.bind(this);
-  }
-
-  updateSearchTerm() {
-    this.setState({ searchTerm: this.searchInput.current.value });
-  }
-
-  matchSearchTerm(obj) {
-    const {
-      id, published, created_at, updated_at, ...rest
-    } = obj;
-    const { searchTerm } = this.state;
-
-    return Object.values(rest).some(
-      value => value.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1,
-    );
-  }
-
-  renderEvents() {
-    const { activeId, events } = this.props;
-    const filteredEvents = events
-      .filter(el => this.matchSearchTerm(el))
-      .sort((a, b) => new Date(b.event_date) - new Date(a.event_date));
-
-    return filteredEvents.map(event => (
-      <li key={event.id}>
-        <Link to={`/events/${event.id}`} className={activeId === event.id ? 'active' : ''}>
-          {event.event_date}
-          {' - '}
-          {event.event_type}
-        </Link>
-      </li>
-    ));
-  }
-
-  render() {
-    return (
-      <section className="eventList">
-        <h2>
+  return (
+    <section className="eventList">
+      <h2>
         Events
-          <Link to="/events/new">New Event</Link>
-        </h2>
+        <Link to="/events/new">New Event</Link>
+      </h2>
+      <input
+        className="search"
+        placeholder="Search"
+        type="text"
+        ref={searchInput}
+        onKeyUp={() => setSearchTerm(searchInput.current.value)}
+      />
+      <ul>
+        {filteredEvents.map((event) => (
+          <li key={event.id}>
+            <Link
+              to={`/events/${event.id}`}
+              className={activeId === event.id ? "active" : ""}
+            >
+              {event.event_date}
+              {" - "}
+              {event.event_type}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
-        <input
-          className="search"
-          placeholder="Search"
-          type="text"
-          ref={this.searchInput}
-          onKeyUp={this.updateSearchTerm}
-        />
-
-        <ul>{this.renderEvents()}</ul>
-      </section>
-    );
-  }
-}
+const matchSearchTerm = (
+  { id, published, created_at, updated_at, ...rest },
+  searchTerm
+) =>
+  Object.values(rest).some(
+    (value) => value.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
+  );
 
 EventList.propTypes = {
   activeId: PropTypes.number,
